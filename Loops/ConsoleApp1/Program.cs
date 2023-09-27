3b58dbb2443eb9eda2cb8dce17d9043de834b3c0
yusifizcode <5ba0gh2@code.edu.az>
2023-09-27 19:55:20 +0400

    Loops

---
+++ Loops/ConsoleApp1/Program.cs
@@ -0,0 +1,117 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1-den M-edek ededler icerisinde 15-e bolunenlerin ededi ortasini hesablayan alqoritm
+            //int M = 45;
+            //double sum = 0;
+            //double count = 0;
+            //double avg = 0;
+
+            //for (int i = 1; i < M; i++)
+            //{
+            //    if (i % 15 == 0)
+            //    {
+            //        sum += i;
+            //        count++;
+            //    }
+            //}
+
+            //avg = sum / count;
+            //Console.WriteLine(avg);
+            #endregion
+
+            #region N-den 100-edek ededler icerisinde 21-e bolunen ededin olub olmadigini tapan alqoritm
+
+            //int n = 84;
+            //bool check = false;
+            //for (int i = n; i < 100; i++)
+            //{
+            //    if (i % 21 == 0)
+            //    {
+            //        check = true;
+            //        break;
+            //    }
+            //}
+
+            //if (check == true)
+            //{
+            //    Console.WriteLine("21e bolunen var");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("21e bolunen yoxdur");
+            //}
+
+
+            #endregion
+
+            #region Verilmis ededin reqemlerinin cemini tapin (Mes: 234 -> 9)
+            //int sum = 0;
+            //int number = 2346;
+            //int digit = 0;
+
+            //while (number > 0)
+            //{
+            //    digit = number % 10;
+            //    sum += digit;
+            //    number = (number - digit) / 10;
+            //}
+
+            //Console.WriteLine(sum);
+            #endregion
+
+            #region Task Verilmiş ədədin polindrom olub olmadığını tapan alqoritm
+            //int num = 121;
+            //int temp = 0;
+            //int rev = num;
+            //int digit = 0;
+
+            //while (num > 0)
+            //{
+            //    digit = num % 10;
+            //    temp = (temp * 10) + digit;
+            //    num = (num - digit) / 10;
+            //}
+
+            //if (temp == rev)
+            //{
+            //    Console.WriteLine("polindorme");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("not polindorme");
+            //}
+            #endregion
+
+            #region Task Verilmis ededin faktorialini tapin.
+            //int num = 5;
+            //int multi = 1;
+
+            //for (int i = 1; i <= num; i++)
+            //{
+            //    multi *= i;
+            //}
+
+            //Console.WriteLine(multi);
+            #endregion
+
+            #region Task Verilmis ededin reqemlerinin hasilini tapin.
+            //int multi = 1;
+            //int number = 2346;
+            //int digit = 0;
+
+            //while (number > 0)
+            //{
+            //    digit = number % 10;
+            //    multi *= digit;
+            //    number = (number - digit) / 10;
+            //}
+
+            //Console.WriteLine(multi);
+            #endregion
+        }
+    }
+}
